64fd105fb131834adf4c8184e45de68b5db56110
g-armand <78274164+g-armand@users.noreply.github.com>
2026-04-16 10:33:44 +0200

    Update setup.py
    Add full dependencies

--- setup.py
+++ setup.py
@@ -9,13 +9,32 @@ setuptools.setup(
 
     version="0.6.0",  # placeholder since Conan installs from git@main
     install_requires=[
-        'onnxruntime>=1.10.0,<2',
-        'ai-edge-litert>=2.0.2,<3',
-        'speexdsp-ns>=0.1.2,<1',
-        'tqdm>=4.0,<5.0',
-        'scipy>=1.3,<2',
-        'scikit-learn>=1,<2',
-        'requests>=2.0,<3',
+        # --- base dependencies ---
+        "onnxruntime>=1.17.1,<2",
+        "ai-edge-litert>=2.0.2,<3",
+        "speexdsp-ns>=0.1.2,<1",
+        "tqdm>=4.0,<5.0",
+        "scipy>=1.3,<2",
+        "scikit-learn>=1,<2",
+        "requests>=2.0,<3",
+
+        # --- ["full"] dependencies ---
+        "mutagen>=1.47.0,<2",
+        "torch>=2.4.0,<3",
+        "torchaudio>=2.4.0,<3",
+        "torchinfo>=1.8.0,<2",
+        "torchmetrics>=0.11.4,<1",
+        "speechbrain>=0.5.16,<1",
+        "audiomentations>=0.38.0,<1",
+        "torch-audiomentations>=0.11.1,<1",
+        "acoustics>=0.2.6,<1",
+        "protobuf>=4.25.3,<5",
+        "onnx>=1.19.1,<2",
+        "pronouncing>=0.2.0,<1",
+        "deep-phonemizer>=0.0.19,<1"
+        "torchcodec>=0.9.1,<1",
+        "onnxscript>=0.5.7,<1",
+        "datasets>=4.1.1,<5",
     ],
     extras_require={},
     author="g-armand",
